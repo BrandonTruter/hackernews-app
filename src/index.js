076a386dfaea37bd9f39a251e5a9e9a5ad2fbe79
Brandon Lee Truter <brandon.truter@ncino.com>
2025-01-03 10:45:23 +0200

    implement graphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ const path = require("path");
 const { getUserId } = require("./utils");
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("apollo-server");
+
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
 };
+
+const pubsub = new PubSub();
 
 const prisma = new PrismaClient();
 
@@ -24,6 +30,7 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
+      pubsub,
       userId: req && req.headers.authorization ? getUserId(req) : null,
     };
   },
